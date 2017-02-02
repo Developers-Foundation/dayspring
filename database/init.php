5c1dda89812c6a680289baf47f5baea49924985d
Nobody Random <iamnobodyrandom@yahoo.com>
2017-02-01 21:55:50 -0500

    inita

--- database/init.php
+++ database/init.php
@@ -20,4 +20,33 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
+/*$sql = "CREATE DATABASE db1;";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}*/
 
+$sql = "
+CREATE TABLE db1(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$sql = "INSERT INTO db1
+VALUES (1,'abc','abc','abc','abc');";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
